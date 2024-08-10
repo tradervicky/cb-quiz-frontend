058cb2308031438e03818030800c30cac509af0b
Vicky Kumar Gupta <vickygupta031@gmail.com>
2024-08-10 23:50:49 +0530

    axios setup completed

--- interfaces/global.ts
+++ interfaces/global.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestHeaders } from 'axios';
 export interface loginProps {
     image: string,
     desc : string,
@@ -6,4 +7,14 @@ export interface loginProps {
 export interface signupProps {
     image: string,
     desc : string,
+}
+
+
+
+export interface MakeApiRequestParams {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'; 
+  url: string;
+  data?: any;
+  params?: any;
+  additionalHeaders?:  Record<string, string>;
 }
---
+++ src/apis/functions/index.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse, AxiosError, AxiosRequestHeaders } from 'axios';
+import { MakeApiRequestParams } from 'interfaces/global';
+const baseUrl = import.meta.env.VITE_REACT_API_URL
+const api = axios.create({
+  baseURL:baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const makeApiRequest = async <T = any>({method,url,data = null,params = null,additionalHeaders = {},
+  }: MakeApiRequestParams): Promise<T>=> {
+  try {
+    const response: AxiosResponse<T> = await api.request({
+      method,
+      url,
+      data,
+      params,
+      headers: {
+        ...api.defaults.headers as Record<string, string>,
+        ...additionalHeaders as Record<string, string>,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    throw axiosError.response ? axiosError.response.data : error;
+  }
+};
